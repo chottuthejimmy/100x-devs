8fb46f9e90455ab1e875b5e09204a5978fdc7f50
Tarun Kumar <srfashions.tarun@gmail.com>
2025-08-25 17:12:13 +0530

    experiments related to async / await

---
+++ week-2/async-await-example.js
@@ -0,0 +1,27 @@
+// Using your helper:
+const setTimeoutPromisified = (duration) =>
+  new Promise((resolve) => setTimeout(() => resolve(), duration));
+
+async function A() {
+  console.log("A start");
+  await setTimeoutPromisified(3000); // pauses A only
+  console.log("A after 3s");
+}
+
+async function B() {
+  console.log("B start");
+  await setTimeoutPromisified(1000); // pauses B only
+  console.log("B after 1s");
+}
+
+// A heartbeat to show the event loop isn't blocked
+let ticks = 0;
+const id = setInterval(() => {
+  console.log("tick", ++ticks);
+  if (ticks === 5) clearInterval(id);
+}, 500);
+
+console.log("before calls");
+A();            // returns a Promise immediately
+B();            // returns a Promise immediately
+console.log("after calls");
---
+++ week-2/callback-hell.js
@@ -0,0 +1,55 @@
+// hi after 1 sec
+// hello after 3sec after step 1
+// hello there after 5 sec after step 2
+
+// cleaner soln w/o promises
+function hi() {
+  console.log("hi");
+  setTimeout(hello, 3000);
+}
+
+function hello() {
+  console.log("hello");
+  setTimeout(helloThere, 5000);
+}
+
+function helloThere() {
+  console.log("hello there");
+}
+
+setTimeout(hi, 1000);
+
+// --------
+
+// With promises
+
+const setTimeoutPromisified = (duration) => {
+  return new Promise((resolve) => setTimeout(() => resolve("kid"), duration));
+};
+
+function output(str) {
+    console.log(str);
+}
+
+setTimeoutPromisified(1000)
+  .then(() => output("hi"))
+  .then(() => setTimeoutPromisified(3000))
+  .then(() => output("hello"))
+  .then(() => setTimeoutPromisified(5000))
+  .then(() => output("hello there"));
+
+
+// -------------
+
+// with async / await
+
+async function solve() {
+  const msg = await setTimeoutPromisified(5000);
+  console.log(msg);
+  await setTimeoutPromisified(3000);
+  console.log("hello");
+  await setTimeoutPromisified(1000);
+  console.log("hello there");
+}
+
+solve();
